f28a183626940a360f96603f1100d1ea0645ec58
Alex Cho <alexsjcho@gmail.com>
2019-04-05 11:43:09 -0700

    Added ultis folder with tools image data and api file

---
+++ src/utils/_DATA.js
@@ -0,0 +1,57 @@
+let tools = {
+  google_calendar: {
+    id: 1,
+    name: "Google Calendar",
+    imageURL:
+      "https://collegeinfogeek.com/wp-content/uploads/2016/08/Google_Calendar_Logo.png",
+    toolURL: "https://www.google.com/calendar"
+  },
+
+  salesforce: {
+    id: 2,
+    name: "Salesforce",
+    imageURL:
+      "https://a.slack-edge.com/7f1a0/plugins/salesforce/assets/service_512.png",
+    toolURL: "https://www.salesforce.com/"
+  },
+
+  zendesk: {
+    id: 3,
+    name: "Zendesk",
+    imageURL:
+      "https://d1eipm3vz40hy0.cloudfront.net/images/social/twitter-zendesk.jpg",
+    toolURL: "https://www.zendesk.com/"
+  },
+
+  mint: {
+    id: 4,
+    name: "Mint",
+    imageURL:
+      "https://www.underconsideration.com/brandnew/archives/mint_2018_icon.png",
+    toolURL: "https://www.mint.com/"
+  },
+
+  marketo: {
+    id: 5,
+    name: "Marketo",
+    imageURL:
+      "https://seeklogo.com/images/M/marketo-logo-05AEB1316A-seeklogo.com.png",
+    toolURL: "https://www.marketo.com/"
+  },
+
+  github: {
+    id: 6,
+    name: "Github",
+    imageURL:
+      "https://github.githubassets.com/images/modules/open_graph/github-mark.png",
+    toolURL: "https://github.com/"
+  },
+
+  trello: {
+    id: 7,
+    name: "Trello",
+    imageURL:
+      "https://www.backupery.com/wp-content/uploads/2018/07/trello-logo.png",
+    toolURL: "https://trello.com/"
+  }
+};
