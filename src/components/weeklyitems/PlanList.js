b943ff190ba4c966715c53baf7a6ffddece0e938
Alex Cho <alexsjcho@gmail.com>
2019-04-09 15:55:02 -0700

    added Map to PlanList successfully

--- src/components/weeklyitems/PlanList.js
+++ src/components/weeklyitems/PlanList.js
@@ -1,23 +1,38 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-// import uuid from "uuid";
+import uuid from "uuid";
 
-import { createPlans } from "../../actions/plans.js";
+import { setWeeklyPlan } from "../../actions/objectives.js";
 import Plan from "./Plan.js";
 
-const mapStateToProps = ({ plans: { plan, planList } }) => ({
-  planList
+const mapStateToProps = ({ plans: { plan, plansList } }) => ({
+  plansList
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ createPlans }, dispatch);
+  bindActionCreators({ setWeeklyPlan }, dispatch);
 
 class PlanList extends Component {
+  onSubmit = submitValue => {
+    const { setWeeklyPlan } = this.props;
+    setWeeklyPlan(submitValue);
+  };
   render() {
+    const { plansList, ...props } = this.props;
     return (
       <Fragment>
-        <Plan />
+        {plansList.map((plan, i) => (
+          <Plan
+            objectiveId={i}
+            key={uuid()}
+            plan={plan}
+            onSubmit={this.onSubmit}
+            {...props}
+          />
+        ))}
+
+        <Plan onSubmit={this.onSubmit} {...props} />
       </Fragment>
     );
   }
