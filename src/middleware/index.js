4b9dc2e9140fb0776ca0b574f53f3fba3ca30bbf
Mario Retana <maretanar@gmail.com>
2019-04-11 17:15:01 -0600

    Added redx dev tools

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
-import logger from "./logger";
-import thunk from "redux-thunk";
-import { applyMiddleware } from "redux";
+import logger from './logger'
+import thunk from 'redux-thunk'
+import { applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default applyMiddleware(thunk, logger);
+export default composeWithDevTools(applyMiddleware(thunk, logger))
